4acf1a95bbfcc73f58ef20fc5522629d7f89f7bd
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-08-07 02:20:57 +0300

    w

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ export default {
 <template>
   <main class="main cont">
     <ul class="items-grid">
+      <li></li>
+
       <li class="item">
         <h2 class="item__title">Helm</h2>
         <ul class="item__list">
@@ -23,54 +25,75 @@ export default {
       </li>
 
       <li class="item">
+        <h2 class="item__title">Amulet</h2>
+        <ul class="item__list">
+          <li>15% incr Melee Attack Speed</li>
+          <li>13% incr Cold Damage</li>
+          <li>+12% Chance to Ignite on Hit</li>
+          <li>+8% Void Res</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Weapon left</h2>
+        <ul class="item__list">
+          <li>Base Attack Rate: 1.05 - Average</li>
+          <li>+48 Melee Damage</li>
+          <li>+15% Chance to inlict Bleed on Hit</li>
+          <li>+57% Chance to inlict Bleed on Hit (?)</li>
+          <li>15% incr Bleed Duration</li>
+          <li>When you hit a bleeding enemy with a melee attack, all bleed stacks on that enemy double in speed</li>
+        </ul>
+      </li>
+
+      <li class="item">
         <h2 class="item__title">Body</h2>
         <ul class="item__list">
-          <li>+194 Armor</li>
-          <li>+8% Endurance</li>
-          <li>+4 Dexterity</li>
-          <li>+12% Fire Res</li>
-        </ul>
-      </li>
-
-      <li class="item">
-        <h2 class="item__title">Boots</h2>
-        <ul class="item__list">
-          <li>+35 Armor</li>
-          <li>10% increased Movement Speed</li>
-          <li>+1 Strength</li>
-          <li>+13% Physical Res</li>
-          <li>+9 Health</li>
-        </ul>
-      </li>
-
-      <li class="item">
-        <h2 class="item__title">Weapon Left-hand</h2>
-        <ul class="item__list">
           <li>qweqwe</li>
           <li>qweqwe</li>
         </ul>
       </li>
 
-      <li></li>
-
       <li class="item">
-        <h2 class="item__title">Weapon Right-hand</h2>
+        <h2 class="item__title">Weapon right</h2>
         <ul class="item__list">
-          <li>qweqwe</li>
-          <li>qweqwe</li>
+          <li>+20% Block Chance</li>
+          <li>+162 Block Effectiveness</li>
+          <li>+107 Armor</li>
+          <li>32 Damage Reflected to Attackers</li>
+          <li>5% Reduced Damage Taken on Block</li>
         </ul>
       </li>
 
       <li class="item">
         <h2 class="item__title">Ring left</h2>
         <ul class="item__list">
-          <li>qweqwe</li>
-          <li>qweqwe</li>
+          <li>45% incr Crit Strike Chance</li>
+          <li>+1 Strength</li>
+          <li>+14% Phys Res</li>
+          <li>+25 Health</li>
         </ul>
       </li>
 
       <li class="item">
-        <h2 class="item__title">Amulet</h2>
+        <h2 class="item__title">Belt</h2>
+        <ul class="item__list">
+          <li>+3 Potion Slots</li>
+          <li>+40 Armor</li>
+          <li>39% incr Poison Damage</li>
+          <li>45% incr Chance to Find Potions</li>
+          <li>11% incr Cooldown Recovery Speed</li>
+          <li>+9% Crit Strike Avoidance</li>
+          <li>+23 Dodge Rating</li>
+          <li>Stats on this item are doubled for 1 sec after using a Potion +1 Potion Slot per equipped Weaver's Will Item</li>
+          <li>49% incr Armor (?)</li>
+          <li>45% of Mana Spent Gained as Ward</li>
+          <li>6% more Damage to Stunned Enemies</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Right right</h2>
         <ul class="item__list">
           <li>qweqwe</li>
           <li>qweqwe</li>
@@ -78,7 +101,23 @@ export default {
       </li>
 
       <li class="item">
-        <h2 class="item__title">Right right</h2>
+        <h2 class="item__title">Gloves</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Boots</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Relic</h2>
         <ul class="item__list">
           <li>qweqwe</li>
           <li>qweqwe</li>
