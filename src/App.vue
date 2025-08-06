f7d689018d3255ed8c44a8c2be55fa2e7e11e515
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-08-07 01:43:08 +0300

    i

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script>
+import Item from './components/Item.vue'
+
+export default {
+  components: {
+    Item,
+  },
+}
+</script>
+
+<template>
+  <main class="main cont">
+    <ul class="items-grid">
+      <li class="item">
+        <h2 class="item__title">Helm</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Body</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Boots</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item item--weapon-left-hand">
+        <h2 class="item__title">Weapon Left-hand</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item item--weapon-right-hand">
+        <h2 class="item__title">Weapon Right-hand</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Ring left</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Amulet</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+
+      <li class="item">
+        <h2 class="item__title">Right right</h2>
+        <ul class="item__list">
+          <li>qweqwe</li>
+          <li>qweqwe</li>
+        </ul>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Item.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: ['title'],
+}
+</script>
+
+<template>
+  <div class="item">
+    <h1>{{ title }}</h1>
+      <ul class="item__list">
+        <li>qweqwe</li>
+        <li>qweqwe</li>
+      </ul>
+  </div>
+</template>
